18e3f7fa3d91e5933c168788443a85298b619753
Dan Kim <itsdanya@gmail.com>
2021-06-28 04:16:20 +0300

    Send advice every 6 hours

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,4 @@
 class Chat < ApplicationRecord
-  scope :stale, -> { where('updated_at < ?', 5.minutes.ago) }
+  scope :stale, -> { where('updated_at < ?', 6.hours.ago) }
   scope :newly_created, -> { where('created_at = updated_at') }
 end
