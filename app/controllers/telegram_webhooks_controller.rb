be47ab24df28dd4e8fdd12de75fa34a7055b7dc1
Dan Kim <itsdanya@gmail.com>
2021-06-28 03:09:00 +0300

    Debug

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,5 +1,7 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
-  def start!(*)
-    puts ''
+  def start!(message)
+    puts message.inspect
+    puts from
+    puts chat
   end
 end
