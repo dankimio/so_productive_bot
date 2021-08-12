e49a8ec02bb78ca5caadddb8b225c42c4232eddf
Dan Kim <itsdanya@gmail.com>
2021-08-12 15:38:28 +0300

    Add welcome message

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,5 +1,10 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def start!
-    Chat.find_or_create_by!(chat_id: chat['id'])
+    @chat = Chat.find_or_create_by!(chat_id: chat['id'])
+
+    Telegram.bot.send_message(
+      chat_id: @chat.chat_id,
+      text: 'Привет! Теперь я буду всеми способами отвлекать тебя от работы'
+    )
   end
 end
